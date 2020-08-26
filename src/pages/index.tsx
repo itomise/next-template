1cc5e2e6436d683ba7d5b98b84762724d3fdea01
ryota.ito <itomise.mizumi@icloud.com>
2020-08-26 14:43:45 +0900

    mod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { NextPage } from 'next'
 
 import Head from 'next/head'
-import { Index } from '~/components/component/Index'
+import { Index } from '~/components/component/index/Index'
 
 const Home: NextPage = () => (
   <>
